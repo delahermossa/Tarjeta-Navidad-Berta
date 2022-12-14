a3832ea3e93821ca5e966b39e9fe683eae468189
delahermossa <delahermossa@gmail.com>
2022-12-14 22:51:34 +0100

    commit

---
+++ felices.js
@@ -0,0 +1,33 @@
+
+window.onload;
+
+setTimeout(mostrar, 3000);
+setTimeout(transparentarImagen, 3000);
+
+function mostrar() {
+    document.getElementById("felicitacion").style.visibility = "visible";
+    document.getElementById("felicitacion").classList.toggle('line-1');
+    document.getElementById("felicitacion").classList.toggle('anim-typewriter');
+    document.getElementById("divAnimacion").style.visibility = "visible";
+    document.getElementById("playPause").style.visibility = "visible";
+    document.getElementById("boton").style.visibility="visible";
+
+
+
+}
+
+function transparentarImagen() {
+    document.getElementById("imagen").style.visibility = "hidden";
+}
+
+
+function showMessage() {
+    alert("¡Te deseo una feliz Navidad!");
+}
+
+function fadeInText() {
+    var text = document.querySelector("#felicitacion");
+    text.style.opacity = 1;
+  }
+
+  
